dc800264d1fc70d7761203cfdf4949fdcbc7d09a
Magnus Edevag <magnus.edevag@gmail.com>
2011-04-19 15:34:19 +0200

    Done with implementation

--- src/main/code/org/codingkata/unit/MyKata.scala
+++ src/main/code/org/codingkata/unit/MyKata.scala
@@ -6,6 +6,17 @@ class MyKata extends BaseKataSolution {
  
     // YOUR IMPLEMENTATION
 	def answer(number: Int): String = {
-		number.toString()
+		
+		var result = ""
+		
+		if (number%3 == 0)
+			result = "fizz"
+		if (number%5 == 0)
+			result += "buzz"
+				
+		if (result.length == 0)
+			result = number.toString()
+			
+		result
 	}
 }
--- src/test/code/org/codingkata/unit/KataTest.scala
+++ src/test/code/org/codingkata/unit/KataTest.scala
@@ -9,14 +9,43 @@ class KataTest extends TestNGSuite {
 	val fizzbuzz = new MyKata
 	
 	@Test
-	def normalNumberGivesNumber() {
+	def testNormalNumberGivesNumber() {
 		
 		val normalNumbers = List(1,2,4,7,8,11);
 		
 		for (normalNumber <- normalNumbers) {
 			assert(normalNumber.toString().equals(fizzbuzz.answer(normalNumber)) )
 		}
+	}
+	
+	@Test
+	def testFizzNumbers() {
+		val fizzNumbers = List(3,6,9,12,33);
 		
+		fizzNumbers.foreach((i : Int) => (
+			assert("fizz".equals(fizzbuzz.answer(i)) )
+		))
+		
+		//fizzNumbers.foreach(assert("fizz".equals(fizzbuzz.answer(_)) ))
+	}
+	
+	@Test
+	def testBuzzNumbers() {
+		val buzzNumbers = List(5,10,55,2015);
+		
+		buzzNumbers.foreach((i : Int) => (
+			assert("buzz".equals(fizzbuzz.answer(i)) )
+		))
+		
+	}
+	
+	@Test
+	def testFizzBuzzNumbers() {
+		val fizzBuzzNumbers = List(15,30,150,60);
+		
+		fizzBuzzNumbers.foreach((i : Int) => (
+			assert("fizzbuzz".equals(fizzbuzz.answer(i)) )
+		))
 		
 	}
 
